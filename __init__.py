5d52d8e3b910429cdb97590a5cf8ec8279396690
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-01 21:32:44 -0400

    wip

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ class Enemies:
         name = "Example"
         desc = "Example is a giant with red skin, likely sunburned.\nThere are rumors that he is very very very strong, but it is unknown if he is friendly."
         attackDamage = (10,20)
+        health = 55
 
 class Shell:
     attack_msg = "%(heSheIt)s %(methodOfAttack)s %(entityName)s, dealing %(damageCount)i damage."
@@ -15,8 +16,18 @@ class Shell:
 class Functions:
     pass
 
-def Start(debug=False):
-    pass
+class Player:
+    class Info:
+        health = 50
+        inventory = []
+        attackStrength = (5,15)
+
+class Program:
+    class Metadata:
+        version = "0.0 (Original Shell) build XXXXXXXX" #tip: for the build put the git commit hash or the svn revision number
+        welcomeMsg = "Hello and welcome to Example version %s! We hope you have a great experience." % self.version
+    def Start(debug=False):
+        pass
 
 if __name__ == "__main__":
-    Start()
+    Program.Start()
