410ea5b9b9c50b6353f49e990344f49c45cd0233
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-01 21:57:50 -0400

    Update theGame.py

--- theGame.py
+++ theGame.py
@@ -1,3 +1,6 @@
 def import(Enemies, Messages, Functions, Rooms, Player, Program, debug):
     global Enemies, Messages, Functions, Rooms, Player, Program, debug
     return
+
+def theGame():
+    pass
