e741089636539b9aea0e4014d8b06cf4b571f4c4
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-01 21:16:26 -0400

    Create shell

---
+++ __init__.py
@@ -0,0 +1,15 @@
+#This is completely untested!!! Made with github online no test.
+
+#See credits in readme.md
+
+class Enemies:
+    pass
+
+class Functions:
+    pass
+
+def Start(debug=False):
+    pass
+
+if __name__ == "__main__":
+    Start()
