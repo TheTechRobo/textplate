a72cec502b8bff755659cee5f775bd405650e86a
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-01 21:26:28 -0400

    wip

--- __init__.py
+++ __init__.py
@@ -3,8 +3,15 @@
 #See credits in readme.md
 
 class Enemies:
-    pass
+    class Example:
+        name = "Example"
+        desc = "Example is a giant with red skin, likely sunburned.\nThere are rumors that he is very very very strong, but it is unknown if he is friendly."
+        attackDamage = (10,20)
 
+class Shell:
+    attack_msg = "%(heSheIt)s %(methodOfAttack)s %(entityName)s, dealing %(damageCount)i damage."
+    playerWin = "%(entityName)s dies and collapses on the ground in front of you. You have %(health)i health remaining."
+    enemyWin = "You die and collapse on the ground. %(entityName)s %(verb)s triumphantly. They had %(health)i health remaining."
 class Functions:
     pass
 
