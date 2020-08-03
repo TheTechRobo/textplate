0fe4de0d5d9cb8e4b162ae572830ca9f53856d76
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-03 12:24:41 -0400

    Update theGame.py

--- theGame.py
+++ theGame.py
@@ -1,8 +1,3 @@
-def i(Enemies, Messages, Functions, Rooms, Player, Program, debugLog):
-    pass
-    """
-    This function is justbecause im too lazy to go into the other file and fix stuff.
-    """
 def theGame(Enemies, Messages, Functions, Rooms, Player, Program, debugLog):
     #DEFINE VARIABLES
     currentRoom = 0
