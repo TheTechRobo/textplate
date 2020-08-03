4eb61abbda6cf26f52776ee18dd21ae39bd496e0
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-03 12:26:35 -0400

    fix

--- theGame.py
+++ theGame.py
@@ -1,3 +1,3 @@
-def theGame(Enemies, Messages, Functions, Rooms, Player, Program, debugLog):
+def theGame(Enemies, Messages, Functions, Rooms, Player, Program, debugLog, debug):
     #DEFINE VARIABLES
     currentRoom = 0
